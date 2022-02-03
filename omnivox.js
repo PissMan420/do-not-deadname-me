f9603913895ed521e2ad78583420e028a47dd409
Vini Dalvino <37189791+PissMan420@users.noreply.github.com>
2022-02-02 23:11:08 -0500

    Added a function to replace element.
    DocumentNode.getElementById did not work on omnvox.
    I had to write my own custom version to find and relace text of a
    element.

--- omnivox.js
+++ omnivox.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name  do not dead name me
 // @description do not get deadnamed on omnivox
+// @namespace dndm
 // @match http*://*.omnivox.ca
 // @grant none
 // @version 1.0
@@ -12,13 +13,35 @@ const deadname = ['Vincent', 'Levasseur']; // Place your deadname here
 const name = ['Aurélie', 'Champoux']; // place your chosen name here
 // ===== Code          =====
 document.addEventListener('load', function (_ev) {
-    document.querySelectorAll('*').forEach( (e)  => {
-        const {innerHTML} = e;
-        if (innerHTML.search(deadname.join(' ')) !== -1)
-            e.innerHTML = innerHTML.replace(name.join(' '));
-        else if (innerHTML.search(deadname[0]) !== -1)
-            e.innerHTML = innerHTML.replace(deadname[0], name[0]); 
-        else if (innerHTML.search(deadname[1]) !== -1)
-            e.innerHTML = innerHTML.replace(deadname[1], name[1]);
-    });
+    setInterval(() => {
+        renameWith('*', function (value) {
+            const {innerHTML} = value;
+            console.log(innerHTML);
+        });
+    }, 500);
 });
+
+/**
+ * Rename all the element using the itateratee
+ * @param options {object} options to find the element
+ * @param renameHandler {(elem) => void} The renaming function
+ */
+function renameWith({ id }, renameHandler=(elem) => void(0)) {
+    const treeWalker = document.createTreeWalker(document.body, 
+        NodeFilter.SHOW_ELEMENT, function treewalker(node) {
+            if (node.id === id)
+                return NodeFilter.FILTER_ACCEPT;
+            else
+                return NodeFilter.FILTER_SKIP;
+        });
+    const nodes = [];
+    let node = true;
+    while (node = treeWalker.nextNode()) // here we use a `=` as it return the 
+                                         // content of the variable.
+        nodes.push(node);
+    for (node of nodes)
+        renameHandler(node);
+          
+}
+
+window.renameWith = renameWith;
